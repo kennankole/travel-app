9a3c07d3f08611291a7af30d7f35c4a1464b52dd
kennankole <kennankole@gmail.com>
2023-10-16 10:52:25 +0300

    Implementing the mobile menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,57 @@
+import { useState } from "react";
 
 
-const NavigationMenu = () => (
-  <nav className="navbar">
-    <ul className="nav-elements">
-      <div className="left">
-        Logo
+const NavigationMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <div className="hamburger-menu">
+        <button
+          className={`hamburger-button ${isOpen ? 'open' : ''}`}
+          onClick={toggleMenu}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </button>
+        {isOpen && (
+          <nav className="navbar mobile">
+          <ul className="nav-elements">
+            <div className="left">
+              Logo
+            </div>
+            <div className="right">
+              <li>Home</li>
+              <li>About</li>
+              <li>Destinations</li>
+              <li>Journeys</li>
+              <li>Contact us</li>
+            </div>
+          </ul>
+        </nav>
+        )}
       </div>
-      <div className="right">
-        <li>Home</li>
-        <li>About</li>
-        <li>Destinations</li>
-        <li>Journeys</li>
-        <li>Contact us</li>
-      </div>
-    </ul>
-  </nav>
-);
+      <nav className="navbar desktop">
+        <ul className="nav-elements">
+          <div className="left">
+            Logo
+          </div>
+          <div className="right">
+            <li>Home</li>
+            <li>About</li>
+            <li>Destinations</li>
+            <li>Journeys</li>
+            <li>Contact us</li>
+          </div>
+        </ul>
+      </nav>
+    </>
+  )
+
+};
 export default NavigationMenu;
