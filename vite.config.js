2acc1d8759731189871f415ea46837cdb05887bd
kennankole <kennankole@gmail.com>
2023-10-20 16:33:19 +0300

    modified vite.config for deployment settings

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/travel-app/',
   plugins: [react()],
+  define: {
+    global: 'window',
+  }
 })
